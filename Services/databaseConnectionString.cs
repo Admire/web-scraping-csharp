279925dc75ba0223bc98874e42786cbd170d646f
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-07-16 12:02:38 +0700

    fix1

--- Services/databaseConnectionString.cs
+++ Services/databaseConnectionString.cs
@@ -5,21 +5,20 @@ using System.Text;
 using web_scraping_csharp.Controllers;
 using web_scraping_csharp.Services;
 
-namespace web_scraping_csharp
-{
-    public partial class Form1 : Form
-    {
-        // địa chỉ kết nối tới server
-        public string getConnection()
-        {
-            string serverName = serverDBname.Text.Trim();
-            string serverPort = serverDBport.Text.Trim();
-            string userName = userDBname.Text.Trim();
-            string password = userDBpassword.Text.Trim();
-            string dbName = DBname.Text.Trim();
+namespace web_scraping_csharp.Services;
 
-            string connectionString = $"server={serverName};port={serverPort};user={userName};password={password};database={dbName}";
-            return connectionString;
-        }
+public class databaseConnectionString
+{
+    // địa chỉ kết nối tới server
+    public string getConnection()
+    {
+        string serverName = new Form1().serverDBname.Text.Trim();
+        string serverPort = new Form1().serverDBport.Text.Trim();
+        string userName = new Form1().userDBname.Text.Trim();
+        string password = new Form1().userDBpassword.Text.Trim();
+        string dbName = new Form1().DBname.Text.Trim();
+
+        string connectionString = $"server={serverName};port={serverPort};user={userName};password={password};database={dbName}";
+        return connectionString;
     }
 }
