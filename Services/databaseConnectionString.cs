5b9cc311e0667a50c2243c752aeaac9bf0ee805b
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-07-16 12:49:36 +0700

    fix2 và thêm thông tin setup

--- Services/databaseConnectionString.cs
+++ Services/databaseConnectionString.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Text;
 using web_scraping_csharp.Controllers;
 using web_scraping_csharp.Services;
-
+using System.Windows.Forms;
 namespace web_scraping_csharp.Services;
 
 public class databaseConnectionString
@@ -12,11 +12,16 @@ public class databaseConnectionString
     // địa chỉ kết nối tới server
     public string getConnection()
     {
-        string serverName = new Form1().serverDBname.Text.Trim();
-        string serverPort = new Form1().serverDBport.Text.Trim();
-        string userName = new Form1().userDBname.Text.Trim();
-        string password = new Form1().userDBpassword.Text.Trim();
-        string dbName = new Form1().DBname.Text.Trim();
+        TextBox serverDBname = Application.OpenForms["Form1"].Controls["serverDBname"] as TextBox;
+        TextBox serverDBport = Application.OpenForms["Form1"].Controls["serverDBport"] as TextBox;
+        TextBox userDBname = Application.OpenForms["Form1"].Controls["userDBname"] as TextBox;
+        TextBox userDBpassword = Application.OpenForms["Form1"].Controls["userDBpassword"] as TextBox;
+        TextBox DBname = Application.OpenForms["Form1"].Controls["DBname"] as TextBox;
+        string serverName = serverDBname.Text.Trim();
+        string serverPort = serverDBport.Text.Trim();
+        string userName = userDBname.Text.Trim();
+        string password = userDBpassword.Text.Trim();
+        string dbName = DBname.Text.Trim();
 
         string connectionString = $"server={serverName};port={serverPort};user={userName};password={password};database={dbName}";
         return connectionString;
